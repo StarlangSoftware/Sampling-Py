3d82a9c7012fa7345c507321b50a29ec9b362611
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-08-10 09:05:35 +0300

    Updated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-Sampling',
-    version='1.0.0',
+    version='1.0.1',
     packages=['Sampling'],
     url='https://github.com/olcaytaner/Sampling-Py',
     license='',
