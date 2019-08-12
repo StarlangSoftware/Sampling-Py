2b97aba1d34a59ce5ca31756af1373b8c98a6082
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-08-12 09:39:49 +0300

    Added documentation.

--- Sampling/KFoldCrossValidation.py
+++ Sampling/KFoldCrossValidation.py
@@ -4,6 +4,19 @@ import random
 
 class KFoldCrossValidation(CrossValidation):
 
+    """
+    A constructor of KFoldCrossValidation class which takes a sample as an array of instances, a K (K in K-fold
+    cross-validation) and a seed number, then shuffles the original sample using this seed as random number.
+
+    PARAMETERS
+    ----------
+    instanceList : list
+        Original sample
+    K : int
+        K in K-fold cross-validation
+    seed : int
+        Random number to create K-fold sample(s)
+    """
     def __init__(self, instanceList: list, K: int, seed: int):
         self.instanceList = instanceList
         random.seed(seed)
@@ -11,6 +24,19 @@ class KFoldCrossValidation(CrossValidation):
         self.N = len(instanceList)
         self.K = K
 
+    """
+    getTrainFold returns the k'th train fold in K-fold cross-validation.
+
+    PARAMETERS
+    ----------
+    k : int 
+        index for the k'th train fold of the K-fold cross-validation
+        
+    RETURNS
+    -------
+    list
+        Produced training sample
+    """
     def getTrainFold(self, k: int) -> list:
         trainFold = []
         for i in range((k * self.N) // self.K):
@@ -19,6 +45,19 @@ class KFoldCrossValidation(CrossValidation):
             trainFold.append(self.instanceList[i])
         return trainFold
 
+    """
+    getTestFold returns the k'th test fold in K-fold cross-validation.
+
+    PARAMETERS
+    ----------
+    k : int
+        index for the k'th test fold of the K-fold cross-validation
+        
+    RETURNS
+    -------
+    list
+        Produced testing sample
+    """
     def getTestFold(self, k: int) -> list:
         testFold = []
         for i in range((k * self.N) // self.K, ((k + 1) * self.N) // self.K):
--- Sampling/StratifiedKFoldCrossValidation.py
+++ Sampling/StratifiedKFoldCrossValidation.py
@@ -4,6 +4,20 @@ import random
 
 class StratifiedKFoldCrossValidation(KFoldCrossValidation):
 
+    """
+    A constructor of StratifiedKFoldCrossValidation class which takes as set of class samples as an array of array of
+    instances, a K (K in K-fold cross-validation) and a seed number, then shuffles each class sample using the
+    seed number.
+
+    PARAMETERS
+    ----------
+    instanceLists : list
+        Original class samples. Each element of the this array is a sample only from one class.
+    K : int
+        K in K-fold cross-validation
+    seed : int
+        Random number to create K-fold sample(s)
+    """
     def __init__(self, instanceLists: list, K: int, seed: int):
         self.instanceLists = instanceLists
         self.N = []
@@ -13,6 +27,19 @@ class StratifiedKFoldCrossValidation(KFoldCrossValidation):
             self.N.append(len(instanceLists[i]))
         self.K = K
 
+    """
+    getTrainFold returns the k'th train fold in K-fold stratified cross-validation.
+
+    PARAMETERS
+    ----------
+    k : int
+        index for the k'th train fold of the K-fold stratified cross-validation
+        
+    RETURNS
+    -------
+    list
+        Produced training sample
+    """
     def getTrainFold(self, k: int) -> list:
         trainFold = []
         for i in range(len(self.N)):
@@ -22,6 +49,19 @@ class StratifiedKFoldCrossValidation(KFoldCrossValidation):
                 trainFold.append(self.instanceLists[i][j])
         return trainFold
 
+    """
+    getTestFold returns the k'th test fold in K-fold stratified cross-validation.
+
+    PARAMETERS
+    ----------
+    k : int
+        index for the k'th test fold of the K-fold stratified cross-validation
+        
+    RETURNS
+    -------
+    list
+        Produced testing sample
+    """
     def getTestFold(self, k: int) -> list:
         testFold = []
         for i in range(len(self.N)):
