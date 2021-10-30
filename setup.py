e36d1f3b3cca18f341fe519cac81d42602eb94e3
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:34:15 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-Sampling',
-    version='1.0.4',
+    version='1.0.5',
     packages=['Sampling'],
     url='https://github.com/StarlangSoftware/Sampling-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
-    description='Data sampling library'
+    description='Data sampling library',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
